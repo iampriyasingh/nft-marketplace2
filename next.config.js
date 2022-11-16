8cf93d622b9c118408ff3e005816ef0ca153495e
priyasingh <priyadiyasistec@gmail.com>
2022-11-16 17:52:50 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["priya-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
+  },
 }
+
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//INTERNAL IMPORT
 
+import { NavBar, Footer } from '../components/componentindex';
+import { NFTMarketplaceProvider } from '../Context/NFTMarketplaceContext';
+
+const MyApp = ({ Component, pageProps }) => (
+<div>
+<NFTMarketplaceProvider>
+<NavBar />
+<Component {...pageProps} />
+<Footer />
+</NFTMarketplaceProvider>
+</div>
+
+);
 export default MyApp
